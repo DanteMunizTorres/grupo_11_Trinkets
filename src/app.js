b6e7d9b81497368ef7eb03f2b004f3dc4aef9eec
Dante Muniz Torres <muniztorresdante@gmail.com>
2021-11-06 18:36:21 -0300

    [MAKE][FIX] cree parcials y arregle ruta de public en app.js'

--- src/app.js
+++ src/app.js
@@ -4,17 +4,23 @@ const port = process.env.PORT || 3030;
 
 const path = require("path");
 
-const publicPath = path.join(__dirname, "./public");
+const publicPath = path.join(__dirname, "../public");
 server.use(express.static(publicPath));
 
+//configuracion EJS
+server.set("views", path.resolve(__dirname, "views"));
+server.set("view engine", "ejs");
+server.use(express.static(path.resolve(__dirname, "public")));
 
-const ENDPOINTS = require('./routes/main-routes')
 
-
+//rutas
+const mainRoutes = require('./routes/main-routes')
+const userRoutes = require('./routes/user-routes')
+const productRoutes = require('./routes/product-routes')
 
 
 server.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'))
+    res.render(path.join(__dirname, '/views/home.ejs'))
 })
 
 server.get('/register.html', (req, res) => {
