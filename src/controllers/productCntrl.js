669d9edc0ccbbbdf286379fafb5eb40bedb8dbe9
Dante Muniz Torres <muniztorresdante@gmail.com>
2021-11-25 19:17:47 -0300

    [MAKE] obtener detalle de producto

--- src/controllers/productCntrl.js
+++ src/controllers/productCntrl.js
@@ -1,9 +1,16 @@
 let fs = require('fs')
 const path = require("path");
 
+//listado de productos
+let productsListJSON = fs.readFileSync(path.join(__dirname,'../data/products.json'), {encoding: 'utf-8'});
+let productList = JSON.parse(productsListJSON);
+
 const controller = {
   product: (req, res) => {
-    res.render('../views/product/product-detail.ejs')
+    let id = req.params.id
+    let productToShow = id -1 
+    // let productDetail = productList.map(product => product.id == id) ESTA FUNCION NO FUNCIONO
+    res.render('../views/product/product-detail.ejs', {productList: productList[productToShow]})
   },
   upload: (req, res) => {
     res.render('../views/product/product-upload.ejs')
@@ -12,10 +19,6 @@ const controller = {
     res.render('../views/product/cart.ejs')
   },
   list: (req, res) => {
-
-    let productsListJSON = fs.readFileSync(path.join(__dirname,'../data/products.json'), {encoding: 'utf-8'});
-    let productList = JSON.parse(productsListJSON);
-    
     res.render('../views/product/products-list.ejs', {productList})
   }
 };
--- src/routes/product-routes.js
+++ src/routes/product-routes.js
@@ -4,8 +4,39 @@ const router = express.Router();
 const productController = require('../controllers/productCntrl');
 
 router.get('/cart', productController.cart);
-router.get('/detail', productController.product);
-router.get('/upload', productController.upload)
+
+
+
+// 1. /products (GET)
+// Listado de productos
 router.get('/list', productController.list)
+
+// 2. /products/create (GET)
+// Formulario de creación de productos
+router.get('/upload', productController.upload)
+
+// 3. /products/:id (GET)
+// Detalle de un producto particular
+router.get('/detail/:id', productController.product);
+
+// 4. /products (POST)
+// Acción de creación (a donde se envía el formulario)
+
+// 5. /products/:id/edit (GET)
+// Formulario de edición de productos
+
+// 6. /products/:id (PUT)
+// Acción de edición (a donde se envía el formulario):
+
+// 7. /products/:id (DELETE)
+// Acción de borrado
+
+
+
+
+
+
+
+
 
 module.exports = router;
