4f18a59150dd2fc8d0ebc29e208b8d4346c393e7
Dante Muniz Torres <muniztorresdante@gmail.com>
2022-01-23 20:16:23 -0300

    [FEAT] configurando multer en user-routes.js, falta arreglar userCntrl.js

--- src/controllers/userCntrl.js
+++ src/controllers/userCntrl.js
@@ -1,3 +1,16 @@
+
+let fs = require('fs')
+const path = require("path");
+
+
+
+//listado de usuarios
+let usersListPath = path.join(__dirname,'../data/users.json')
+let usersListJSON = fs.readFileSync(usersListPath, {encoding: 'utf-8'});
+let usersList = JSON.parse(usersListJSON);
+
+
+
 const controller = {
   getRegister: (req, res) => {
     res.render('../views/user/register')
@@ -6,7 +19,29 @@ const controller = {
     res.render('../views/user/login')
   },
   newUser: (req, res) => {
-    
+    let newId = usersList.length + 1;
+    let password = req.body.password;
+    let encryptedPassword = bcrypt.hashSync(password, 10);
+    let avatarImg = req.body.avatar-img;
+    let newUser = {
+      id: newId,
+      name: req.body.name,
+      surname: req.body.surname,
+      dni: req.body.dni,
+      city: req.body.city,
+      email: req.body.email, 
+      password: encryptedPassword,
+      terms: req.body.terms,
+      avatarImg: avatarImg
+    }
+
+    usersList.push(newUser);
+
+    let newUsersList = JSON.stringify(usersList);
+
+    fs.writeFileSync(usersListPath, newUsersList)
+
+    res.redirect('/')
   }
 };
 
--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+let fs = require('fs')
+const path = require("path");
+
+const multer = require('multer');
+let multerDiskStorage = multer.diskStorage({
+  destination: (req, file, cb)=>{
+    let folder = path.join(__dirname, '../../public/img/users');
+    cb(null, folder);
+  },
+  filname: (req, file, cb)=>{
+    let imgName = 'avatar_' + Date.now + path.extname(file.originalName);
+    cb(null, imgName)
+  }
+})
+let fileUpload = multer({storage: multerDiskStorage})
+
 const userController = require('../controllers/userCntrl');
 
 router.get('/register', userController.getRegister);
 router.get('/login', userController.getLogin);
 
-router.post('/new-user', userController.newUser)
+router.post('/new-user', fileUpload.single(avatar-img), userController.newUser)
 
 
 
