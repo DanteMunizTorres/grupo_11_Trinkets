ea23d9bba933466033d8fbada1777ef0b0fd8bdb
Dante Muniz Torres <muniztorresdante@gmail.com>
2022-01-25 12:02:42 -0300

    [FEAT] agregue modelo de User en json como explica en el video de login completo

--- src/controllers/userCntrl.js
+++ src/controllers/userCntrl.js
@@ -5,6 +5,9 @@ let bcrypt = require('bcryptjs')
 const { validationResult } = require('express-validator')
 
 
+const User = require('../models/User')
+
+
 //listado de usuarios
 let usersListPath = path.join(__dirname,'../data/users.json')
 let usersListJSON = fs.readFileSync(usersListPath, {encoding: 'utf-8'});
@@ -23,22 +26,24 @@ const controller = {
   newUser: (req, res) => {
     //validacion de los campos
     let errors = validationResult(req)
-    if (errors.isEmpty()) {
+    if (errors.isEmpty()) {      
       //si esta todo bien genera el usuario
-      let newId = usersList.length + 1;
+//TENIENDO EL MODEL DE USER.JSON
 
+
+
+//ANTES DE TENER EL MODELO
+      let newId = usersList.length + 1;
       //encriptacion de la contraseña
       let password = req.body.password;
       let encryptedPassword = bcrypt.hashSync(password, 10);
-
       //checkeo de la imagen de usuario, si esta vacia se coloca imagen default
       let avatarImg
       if (req.file){
         avatarImg = req.file.filename;
       } else {
         avatarImg = 'default.svg'
-      }
-      
+      }     
       //asignacion de valores que vienen del formulario
       let newUser = {
         id: newId,
@@ -51,7 +56,6 @@ const controller = {
         terms: req.body.terms,
         avatarImg: avatarImg
       }
-
       //escritura del json de usuarios
       usersList.push(newUser);
       let newUsersList = JSON.stringify(usersList, null, ' ');
