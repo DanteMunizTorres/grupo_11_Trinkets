a75197e3052f9385f3ecd6f937b0ec604977f50b
Dante Muniz Torres <muniztorresdante@gmail.com>
2022-02-22 13:00:22 -0300

    [FIX] user edit ahora modifica usuario de session

--- src/controllers/userCntrl.js
+++ src/controllers/userCntrl.js
@@ -23,38 +23,12 @@ const multer = require('multer');
 
 
 
-
-
-
-
-//listado de usuarios
-// let usersListPath = path.join(__dirname,'../data/users.json')
-// let usersListJSON = fs.readFileSync(usersListPath, {encoding: 'utf-8'});
-// let usersList = JSON.parse(usersListJSON);
-
-// const User = require('../models/User.js');
-
-
 let User = require('../database/models/User')
 
 //esto va funcionando
 // User.findAll().then(result => console.log(result)) 
-
 // User.findByPk(3).then(result => console.log(result))
 
-
-
-
-//ESTO LO HICE PARA COMPROBAR LA CONEXION CON LA BASE DE DATOS Y SI FUNCIONA
-// let test
-// User.findOne({
-//   where: {
-//     email: 'srickman1@va.gov'
-//   }
-// }).then(result => {console.log(result.email); return result = test})
-// console.log('RESULTADO DE TEST                                                    TEST RESULDATO')
-// console.log(test)
-// EL PROBLEMA ERA EL MANEJO CORRECTO DE LAS PROMESAS Y SUS REASULTADOS
 
 const controller = {
   getRegister: (req, res) => {
@@ -120,7 +94,6 @@ const controller = {
   },
   loginProcess: (req, res) => {
     // console.log(req.body.email);
-    // let userToLogin = User.findByField('email', req.body.email); //FORMA CON JSON
 
     User.findOne({
       where: {
@@ -133,7 +106,7 @@ const controller = {
           delete userToLogin.password;
           req.session.userLogged = userToLogin;
           if (req.body.remember) {
-            res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 10 })
+            res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 240 })
           }
 
           return res.redirect('/user/profile')
@@ -160,7 +133,6 @@ const controller = {
       }
     })//aca termina el ultimo .then
 
-    //aca termina loguin procces
   },
   userProfile: (req, res) => {
     // console.log('ESTO VIENE EN USERLOGGED-----------------------------------------------')
@@ -197,7 +169,7 @@ const controller = {
             city: req.body.city,
             email: req.body.email,
             password: bcrypt.hashSync(req.body.password, 10),
-            // imgUser: avatarImg
+            
           } 
           if (req.file) {
             userNewInfo.imgUser = req.file.filename;
@@ -208,13 +180,31 @@ const controller = {
             }
           })
           .then(()=> {
-            res.redirect('/user/profile')
+            //cambiar info de cookies y session
+            // console.log(result);
+
+            // res.clearCookie('userEmail');
+
+            User.findOne({
+              where: {
+                id: id
+              }
+            })
+            .then((userToLogin) => {
+              console.log(id);
+              console.log(userToLogin);
+                  req.session.userLogged = userToLogin;
+                  res.cookie('userEmail', userToLogin.email, { maxAge: (1000 * 60) * 240 })
+                  
+                  return res.redirect('/user/profile')
+        
+                })
           })
           .catch(err => console.log('----------------HUBO UN ERROR: ' + err))
 
 
     } else {
-      res.render('../views/user/register', { errors: errors.mapped(), old: req.body })
+      res.render('../views/user/edit.ejs', { errors: errors.mapped(), old: req.body, user: req.session.userLogged })
     }
   }
 };
